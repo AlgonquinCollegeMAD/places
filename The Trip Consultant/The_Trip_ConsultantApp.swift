2f62e84718969745f2ba15de65a3b3c5e7d17acf
“Vladimir <vladimircezar@icloud.com>
2023-10-02 16:39:25 -0400

    Initial Commit

---
+++ The Trip Consultant/The_Trip_ConsultantApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Trip_ConsultantApp.swift
+//  The Trip Consultant
+//
+//  Created by Vladimir Cezar on 2023-10-02.
+//
+
+import SwiftUI
+
+@main
+struct The_Trip_ConsultantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
