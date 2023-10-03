271092f41e71255388be4930ea4cfabf56c8968e
“Vladimir <vladimircezar@icloud.com>
2023-10-03 10:38:57 -0400

    Add pictures to list

--- The Trip Consultant/ContentView.swift
+++ The Trip Consultant/ContentView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
   
-  var listOfPlaces = Provider.shared.allPlaces() 
+  var listOfPlaces = Provider.shared.allPlaces().sorted { $0.name < $1.name }
   
   var body: some View {
     List(listOfPlaces) { place in
-      Text(place.name)
+      HStack {
+        Image(place.picture)
+          .resizable()
+          .frame(width: 30, height: 30)
+        Text(place.name)
+      }
     }
       .padding()
   }
