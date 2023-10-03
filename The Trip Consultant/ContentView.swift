b6d33366b0ca0f45d004f5dad8099a47efba6899
“Vladimir <vladimircezar@icloud.com>
2023-10-03 10:40:24 -0400

    Add navigation stack

--- The Trip Consultant/ContentView.swift
+++ The Trip Consultant/ContentView.swift
@@ -12,15 +12,18 @@ struct ContentView: View {
   var listOfPlaces = Provider.shared.allPlaces().sorted { $0.name < $1.name }
   
   var body: some View {
-    List(listOfPlaces) { place in
-      HStack {
-        Image(place.picture)
-          .resizable()
-          .frame(width: 30, height: 30)
-        Text(place.name)
+    NavigationStack{
+      List(listOfPlaces) { place in
+        HStack {
+          Image(place.picture)
+            .resizable()
+            .frame(width: 30, height: 30)
+          Text(place.name)
+        }
       }
-    }
       .padding()
+      .navigationTitle("Best places to visit")
+    }
   }
 }
 
