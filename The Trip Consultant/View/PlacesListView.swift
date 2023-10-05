d645ed1913b82b704c791ea54489f4d08a305142
Vladimir Cezar <vcezar@distillmobile.com>
2023-10-05 17:33:25 -0400

    Add loading data view...

--- The Trip Consultant/View/PlacesListView.swift
+++ The Trip Consultant/View/PlacesListView.swift
@@ -1,44 +1,52 @@
 import SwiftUI
 
 struct PlacesListView: View {
-  
-  var listOfPlaces = Provider.shared.allPlaces().sorted { $0.name < $1.name }
-  
-  var body: some View {
-    ZStack {
-      Color.cyan
-        NavigationStack{
-          List {
+    
+    @State var listOfPlaces = [Place]()
+    
+    var body: some View {
+        if listOfPlaces.count == 0 {
             VStack {
-              ForEach(listOfPlaces) { place in
-                ZStack {
-                  PlaceCardView(place: place)
-                    .padding()
-                    .background(Color.secondary.opacity(0.2))
-                    .cornerRadius(20.0)
-                    .padding(.bottom, 30)
-                    
-        
-                  NavigationLink {
-                    Text(place.name)
-                  } label: {
-                    EmptyView()
-                  }.frame(width: 0).opacity(0)
-                }
-                 .listRowSeparator(.hidden)
-              }
+                ProgressView()
+                Text("Loading data...")
+                .onAppear() {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                        listOfPlaces = Provider.shared.allPlaces().sorted { $0.name < $1.name }
+                    }
             }
-          }
-          .navigationTitle("Places")
-          .foregroundColor(.primary)
-          .listStyle(.plain)
+            }
+        } else {
+            NavigationStack {
+                List {
+                    VStack {
+                        ForEach(listOfPlaces) { place in
+                            ZStack {
+                                PlaceCardView(place: place)
+                                    .padding()
+                                    .background(Color.secondary.opacity(0.2))
+                                    .cornerRadius(20.0)
+                                    .padding(.bottom, 30)
+                                
+                                NavigationLink {
+                                    Text(place.name)
+                                } label: {
+                                    EmptyView()
+                                }.frame(width: 0).opacity(0)
+                            }
+                            .listRowSeparator(.hidden)
+                        }
+                    }
+                }
+                .navigationTitle("Places")
+                .foregroundColor(.primary)
+                .listStyle(.plain)
+            }
         }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    PlacesListView()
-  }
+    static var previews: some View {
+        PlacesListView()
+    }
 }
