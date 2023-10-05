9136288d7766d3564a066214fe6bb835f3c4d8d3
Vladimir Cezar <vcezar@distillmobile.com>
2023-10-05 17:43:06 -0400

    Fixes navigation issue on returning to the main list

---
+++ The Trip Consultant/View/PlaceMapView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct PlaceMapView: View {
+    
+    var place: Place
+    
+    var body: some View {
+        Text(place.name)
+    }
+}
--- The Trip Consultant/View/PlacesListView.swift
+++ The Trip Consultant/View/PlacesListView.swift
@@ -1,52 +1,46 @@
 import SwiftUI
 
 struct PlacesListView: View {
-    
-    @State var listOfPlaces = [Place]()
-    
-    var body: some View {
-        if listOfPlaces.count == 0 {
-            VStack {
-                ProgressView()
-                Text("Loading data...")
-                .onAppear() {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
-                        listOfPlaces = Provider.shared.allPlaces().sorted { $0.name < $1.name }
-                    }
+  
+  @State var listOfPlaces = [Place]()
+  
+  var body: some View {
+    if listOfPlaces.count == 0 {
+      VStack {
+        ProgressView()
+        Text("Loading data...")
+          .onAppear() {
+            print("SHOW")
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+              listOfPlaces = Provider.shared.allPlaces().sorted { $0.name < $1.name }
+//            }
+          }
+      }
+    } else {
+      NavigationStack {
+        List {
+          ForEach(listOfPlaces) { place in
+            NavigationLink {
+              Text(place.name)
+            } label: {
+              PlaceCardView(place: place)
+                .padding()
+                .background(Color.secondary.opacity(0.2))
+                .cornerRadius(20.0)
             }
-            }
-        } else {
-            NavigationStack {
-                List {
-                    VStack {
-                        ForEach(listOfPlaces) { place in
-                            ZStack {
-                                PlaceCardView(place: place)
-                                    .padding()
-                                    .background(Color.secondary.opacity(0.2))
-                                    .cornerRadius(20.0)
-                                    .padding(.bottom, 30)
-                                
-                                NavigationLink {
-                                    Text(place.name)
-                                } label: {
-                                    EmptyView()
-                                }.frame(width: 0).opacity(0)
-                            }
-                            .listRowSeparator(.hidden)
-                        }
-                    }
-                }
-                .navigationTitle("Places")
-                .foregroundColor(.primary)
-                .listStyle(.plain)
-            }
+            .listRowSeparator(.hidden)
+          }
         }
+        .navigationTitle("Places")
+        .foregroundColor(.primary)
+        .listStyle(.plain)
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        PlacesListView()
-    }
+  static var previews: some View {
+    PlacesListView()
+  }
 }
