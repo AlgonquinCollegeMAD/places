59b7b856d85d6cdc05ab3f543463a368143b933c
Vladimir Cezar <vcezar@distillmobile.com>
2023-10-06 11:14:05 -0400

    Add initial map view

--- The Trip Consultant/View/PlaceMapView.swift
+++ The Trip Consultant/View/PlaceMapView.swift
@@ -1,11 +1,49 @@
 import SwiftUI
+import MapKit
 
 struct PlaceMapView: View {
     
     var place: Place
     
     var body: some View {
-        Text(place.name)
+      ZStack {
+        VStack {
+          Map(
+            coordinateRegion: .constant(
+              MKCoordinateRegion(
+                center: CLLocationCoordinate2D(
+                  latitude: place.latitude,
+                  longitude: place.longitude
+                ),
+                span: MKCoordinateSpan(
+                  latitudeDelta: 0.5,
+                  longitudeDelta: 0.5
+                )
+              )
+            ),
+            interactionModes: []
+          )
+          .frame(width: 400, height: 300)
+          .edgesIgnoringSafeArea(.all)
+          Spacer()
+        }
+        VStack(alignment: .leading) {
+          Image(place.picture)
+            .resizable()
+            .frame(height: 250)
+            .aspectRatio(contentMode: .fit)
+            .clipShape(
+              Circle()
+            )
+            .overlay(
+              Circle()
+                .stroke(lineWidth: 16)
+                .foregroundColor(Color.gray.opacity(0.1))
+            )
+            .padding(.top, 50)
+          Spacer()
+        }
+      }
     }
 }
 
